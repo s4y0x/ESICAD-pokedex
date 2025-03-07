1bfab036ba2e7304b2191eca1aa9c07d0d0c54a0
s4y0x <126882041+s4y0x@users.noreply.github.com>
2025-03-07 16:37:41 +0100

    1st commit

--- database-connection.php
+++ database-connection.php
@@ -1,17 +1,21 @@
 <?php
 /*
 Ce fichier permet de définir un objet de connexion $databaseConnection 
-que vous pouvez utiliser dans chaque page qui nécessite de faire une requête au SGBD
-Il suffit d'utiliser l'instruction require_once("database_connection.php") 
-pour que la variable $databaseConnection soit utilisable dans votre page
+que vous pouvez utiliser dans chaque page qui nécessite de faire une requête au SGBD.
+Il suffit d'utiliser require_once("config.php") pour que la variable $databaseConnection soit utilisable.
 */
+
 global $databaseConnection;
 
-/* A COMPLETER : remplacer les paramètres pour se connecter à votre base de données de pokémon */
 $databaseConnection = mysqli_connect(
     'localhost',
-    "root",
+    'root',
     null,
-    "pokemon",
-    "3306"
+    'pokemon',
+    '3306'
 );
+
+if (!$databaseConnection) {
+    die("Erreur de connexion à la base de données : " . mysqli_connect_error());
+}
+?>
--- list.php
+++ list.php
@@ -1,36 +1,17 @@
-<!-- 
-    Ce fichier représente la page de liste de tous les pokémons.
--->
 <?php
 require_once("head.php");
-?>
-<?php
-require_once ("database-connection.php") ;
-?>
-<?php
-if ($result = mysqli_query($databaseConnection, "SELECT * FROM pokemon")) {
+require_once("database-connection.php");
 
-  // Seek to row number 15
-  mysqli_data_seek($result,14);
-
-  // Fetch row
-  $row=mysqli_fetch_row($result);
-
-  printf ("ID: %s <br>NOM: %s<br>URL: %s<br>PV: %s<br>PtsAttaque: %s<br>PtsDefense: %s<br>PtsSpecial: %s<br>PtsVitesse: %s<br>DateAjout: %s<br>idType2:  %s\r<br>idType1 %s", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9], $row[10]);
-
-  // Free result set
-  mysqli_free_result($result);
+$result = $databaseConnection -> query ( "SELECT * FROM pokemon");
+while ( $row =$result ->fetch_assoc()) {
+    echo '<p>'.$row['nomPokemon'].'</p>';
+    echo '<p>'.$row["idPokemon"].'</p>';
+    echo '<img src="'. $row["urlPhoto"] .'"alt="">' ;
+   
 }
-?>
-
-<pre>
-    &lt;
-    A REMPLACER PAR VOTRE CODE POUR CHARGER ET AFFICHER DANS UN TABLEAU LA LISTE DES POKEMONS PAR LEUR NOM.
-    CHAQUE POKEMON DOIT ETRE CLIQUABLE POUR NAVIGUER SUR UNE PAGE OU L'ON AFFICHE SON IMAGE ET L'ENSEMBLE DE SES CARACTERISTIQUES 
-    &gt;
-    </pre>
 
 
-<?php
-require_once("footer.php");
+
+
+require_once("footer.php")
 ?>
